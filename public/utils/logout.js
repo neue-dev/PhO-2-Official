356bd2e07d6ee8eade2d3da6eaae6b1e128d0a71
modavid <modavid.1964@gmail.com>
2024-10-28 04:38:11 +0800

    Add fomantic-ui dependency for pending UI refactor.

--- public/utils/logout.js
+++
@@ -1,4 +0,0 @@
-const logoutButton = document.querySelectorAll('.logout-button');
-logoutButton.forEach(button => button.addEventListener('click', e => {
-  createXHR('./auth/logout', 'POST', {}, () => { location.href = '/' });
-}));
