dd753a6714d52bcdd987b5a320cd267f7b64fd3f
modavid <modavid.1964@gmail.com>
2024-10-30 09:47:27 +0800

    Update trademark.

--- src/public/utils/trademark.js
+++ src/public/utils/trademark.js
@@ -1,8 +1,17 @@
-// Append the trademark
-const trademark = document.createElement('a');
 
-trademark.href = "https://www.linkedin.com/in/m-david-6192222b2/";
-trademark.target = "_blank";
-trademark.innerHTML = `<div class="trademark">website by: <span class="blue-text text-accent-2"><b>Mo David</b></span></div>`;
+(() => {
 
-document.body.appendChild(trademark);
+	// Create the trademark
+	const trademark = DOM.link()
+		.ref('https://www.linkedin.com/in/m-david-6192222b2/')
+		.a('target', '_blank')
+		.append(
+			DOM.div().c('trademark').t('website by: ')
+				.append(
+					DOM.span().append(DOM.b().t('Mo David'))
+				)
+		)
+
+	// Append the trademark
+	document.body.appendChild(trademark);
+})()
