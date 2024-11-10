eed167c80a109f05cbefc83de36e1dd38480fbb5
modavid <modavid.1964@gmail.com>
2024-11-11 07:22:43 +0800

    Implement command autocomplete and recommendation for command palette.

--- src/public/resources/common/palette.js
+++ src/public/resources/common/palette.js
@@ -1,7 +1,7 @@
 /**
  * @ Author: Mo David
  * @ Create Time: 2024-11-07 13:15:20
- * @ Modified time: 2024-11-10 06:52:25
+ * @ Modified time: 2024-11-11 07:22:20
  * @ Description:
  * 
  * Handles the command palette interface.
@@ -11,6 +11,11 @@ const Palette = (() => {
 	
 	// Interface
 	const _ = {};
+
+	// Some constants
+	const WHITE = 'rgb(255, 255, 255)'
+	const BLUE = 'rgb(65, 80, 200)'
+	const GREEN = 'rgb(102, 255, 102)'
 
 	// The commands
 	const commands = [];
@@ -26,17 +31,20 @@ const Palette = (() => {
 			.c('ui', 'input', 'palette')
 			.s({ color: 'white' })
 			.a('contenteditable', true)
-			// .a('placeholder', 'type command here')
 			.a('spellcheck', false)
 
-	// Helper function
+	// The cursor object
+	const palette_cursor = DOM.cursor(palette_input);
+	const palette_shadow = DOM.div().c('ui', 'input', 'palette', 'shadow')
+
+	// Helper function, we're just saving it as a property of the element
 	palette_input.input = () => palette_input.textContent.trim()
 	
 	// List of commands
 	const palette_table = DOM.stateful_table().s({ border: 'none' })
 	
 	// Create the palette element structure
-	palette_element.append(palette_input, DOM.div().c('ui', 'divider'), DOM.br(), palette_table)
+	palette_element.append(palette_input, palette_shadow, DOM.div().c('ui', 'divider'), DOM.br(), palette_table)
 
 	// Append to the dom
 	DOM.append(palette_element);
@@ -45,14 +53,22 @@ const Palette = (() => {
 	const palette = DOM.stateful_modal(palette_element);
 
 	// Focusing and unfocusing 
+	DOM.keybind({ keyCode: 9 }, () => (_.autocomplete()))
 	DOM.keybind({ ctrlKey: true, key: 'p' }, () => (palette.modal_toggle(), palette_input.toggle(), _.display_commands()));
 	DOM.keybind({ key: 'Escape' }, () => (palette.modal_close(), palette_input.blur()));
 
 	// Other key event listeners
 	palette_input.listen('input', (e) => (
+
+		// Run command on enter, otherwise just filter displayed commands
 		[ 'insertLineBreak', 'insertParagraph' ].includes(e.inputType)  
-			? (_.run_command(e.target.input()), e.target.textContent = e.target.input().replaceAll('\n', ''))
-			: (_.filter_commands(e.target.input()), _.display_commands())))
+			? (_.run_command(palette_input.input()), palette_input.textContent = palette_input.input().replaceAll('\n', ''), palette_cursor.end())
+			: (_.filter_commands(palette_input.input()), _.display_commands()),
+		
+		// Change the recommendation shadow
+		palette_input.input().length 
+			? palette_shadow.t(palette_input.recommendation) 
+			: palette_shadow.t('')))
 
 	/**
 	 * Creates a mapper for palette rows with the filter_key highlighted.
@@ -66,11 +82,24 @@ const Palette = (() => {
 		(command) => (
 			((row, identifier) => (
 
+				// Fill in the text on click
+				row.listen('click', (e) => (
+					palette_input.textContent = command.names[0],
+					palette_input.dispatch('input'),
+					palette_input.focus(),
+					palette_cursor.end()
+				)),
+
 				// Create the command identifier (labels)
 				identifier.t(command.names.map(name =>
 					name === filter_key.trim()
-						? (row.s({ color: 'rgb(102, 255, 102)' }), identifier.s({ backgroundColor: 'rgb(102, 255, 102)' }), name)
-						: name.replace(filter_key, DOM.span().s({ color: 'rgb(65, 80, 200)' }).t(filter_key).t())).join('/')),
+						? (identifier.s({ backgroundColor: GREEN }),
+							row.s({ color: GREEN }), 
+							name)
+						: name.replace(
+								filter_key, 
+								DOM.span().s({ color: BLUE }).t(filter_key).t()
+							)).join('/')),
 
 				// Actual command row information
 				row.s({ height: '2.4em' }).append(
@@ -78,7 +107,10 @@ const Palette = (() => {
 					DOM.td().c('spacer').s({ width: '1em' }),
 					DOM.td().t(command.description).s({ opacity: 0.33 }))
 			))
-			(DOM.tr(), DOM.div().c('ui', 'basic', 'compact', 'label').s({ backgroundColor: 'rgba(255, 255, 255, 0.75)' }))
+
+			// Create the row and the identifier
+			(DOM.tr().c('hoverable-row'), 
+			DOM.div().c('ui', 'basic', 'compact', 'label').s({ backgroundColor: WHITE, opacity: 0.7, fontWeight: 400 }))
 		)
 	)
 
@@ -101,6 +133,18 @@ const Palette = (() => {
 	)
 
 	/**
+	 * Performs autocomplete and fills the text content of the input with the recommendation.
+	 * 
+	 * @return	The api. 
+	 */
+	_.autocomplete = () => (
+		palette_input.recommendation.length 
+			? (palette_input.textContent = palette_input.recommendation, palette_input.dispatch('input'), palette_cursor.end())
+			: null,
+		_
+	)
+
+	/**
 	 * Runs a given command with the given name, if it exists.
 	 * If more than once match occurs, the first one is chosen.
 	 * 
@@ -108,7 +152,6 @@ const Palette = (() => {
 	 * @return			The api.
 	 */
 	_.run_command = (name) => (
-		console.log(name),
 		((matches) => (
 			matches.length ? matches[0].action() : console.error('Command not found')
 		))(commands.filter(command => command.names.includes(name))),
@@ -141,14 +184,14 @@ const Palette = (() => {
 		((matches) => (
 
 			// Mock autocomplete
-			palette_input.a('data-content', matches.length ? filter_key === '' ? '' : matches[0] : ''), 
+			palette_input.recommendation = matches.length ? filter_key === '' ? '' : matches[0] : '', 
 
 			// Table filter
 			palette_table.mapper = palette_mapper_factory(filter_key),
 			palette_table.table_filter((command) => command.names.some(name => name.includes(filter_key)))
 
 		// Compute all the matching names
-		))(commands.map(command => command.names.filter(name => name.includes(filter_key))).flat()),
+		))(commands.map(command => command.names.filter(name => name.startsWith(filter_key))).flat()),
 		_
 	)
 
